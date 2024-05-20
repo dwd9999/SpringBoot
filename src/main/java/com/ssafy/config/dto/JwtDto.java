12793cb7d20b5c07cc84daa333af579ab4a50d01
dwd9999 <ojw1010@gmail.com>
2024-05-20 17:41:39 +0900

    Feat: JWT DTO 추가

--- src/main/java/com/ssafy/config/dto/JwtDto.java
+++ src/main/java/com/ssafy/config/dto/JwtDto.java
@@ -1,12 +1,11 @@
 package com.ssafy.config.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
 @RequiredArgsConstructor
 public class JwtDto {
     private final String access_token;
