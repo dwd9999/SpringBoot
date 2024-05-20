c1e8b8bfae48c33ea035a044e17b6b2fc5ca5b4d
dwd9999 <ojw1010@gmail.com>
2024-05-20 17:39:43 +0900

    Feat: 인증/인가 관련 Controller 분리

---
+++ src/main/java/com/ssafy/user/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.ssafy.user.controller;
+
+import com.ssafy.user.dto.LoginRequestDto;
+import com.ssafy.user.dto.LoginResponseDto;
+import com.ssafy.user.dto.RegisterRequestDto;
+import com.ssafy.user.model.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Validated RegisterRequestDto registerRequestDto) {
+        authService.register(registerRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody @Validated LoginRequestDto loginRequestDto) {
+        LoginResponseDto result = authService.login(loginRequestDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
--- src/main/java/com/ssafy/user/controller/UserController.java
+++ src/main/java/com/ssafy/user/controller/UserController.java
@@ -17,7 +17,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
@@ -26,41 +25,19 @@ public class UserController {
     private final UserService userService;
     private final JwtService jwtService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Validated RegisterRequestDto registerRequestDto) {
-        log.info("Register Request Arrived! Request: {}", registerRequestDto);
-        userService.register(registerRequestDto);
-        log.info("Register Success!");
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody @Validated LoginRequestDto loginRequestDto) {
-        log.info("Login Request Arrived! Request: {}", loginRequestDto);
-        LoginResponseDto result = userService.login(loginRequestDto);
-        result.setAccessToken(jwtService.createAccessToken("userId", result.getId()));
-        result.setRefreshToken(jwtService.createRefreshToken("userId", result.getId()));
-        log.info("Login Success! Response: {}", result);
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @GetMapping("/info/{userId}")
-    public ResponseEntity<UserInfoDto> getUserInfo(@PathVariable("userId") String userId,
-                                                   HttpServletRequest request) {
-        log.info("Get UserInfo Request Arrived! Request: {}", userId);
-        if (!jwtService.checkToken(request.getHeader("access-token"))) {
-            throw new UnauthorizedTokenException();
-        }
-        log.info("Token Checked!");
-
-        UserInfoDto result = userService.getUserInfo(userId);
-
-        if (result == null) {
-            throw new UserNotFoundException();
-        }
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+//    @GetMapping("/info/{userId}")
+//    public ResponseEntity<UserInfoDto> getUserInfo(@PathVariable("userId") String userId,
+//                                                   HttpServletRequest request) {
+//        if (!jwtService.checkToken(request.getHeader("access-token"))) {
+//            throw new UnauthorizedTokenException();
+//        }
+//
+//        UserInfoDto result = userService.getUserInfo(userId);
+//
+//        if (result == null) {
+//            throw new UserNotFoundException();
+//        }
+//
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
 }
