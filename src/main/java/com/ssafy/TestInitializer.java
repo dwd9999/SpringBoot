2cbc892ed1ec4284f4a8c0be64a6ccace3b04c79
dwd9999 <ojw1010@gmail.com>
2024-05-22 17:28:12 +0900

    Feat: 게시글 테스트 데이터 추가

--- src/main/java/com/ssafy/TestInitializer.java
+++ src/main/java/com/ssafy/TestInitializer.java
@@ -1,5 +1,8 @@
 package com.ssafy;
 
+import com.ssafy.board.dto.WriteRequestDto;
+import com.ssafy.board.model.mapper.BoardMapper;
+import com.ssafy.user.dto.RegisterRequestDto;
 import com.ssafy.user.model.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class TestInitializer implements CommandLineRunner {
 
     private final UserMapper userMapper;
+    private final BoardMapper boardMapper;
     private final PasswordEncoder encoder;
 
     @Override
@@ -20,5 +24,23 @@ public class TestInitializer implements CommandLineRunner {
                 encoder.encode("test"),
                 "test",
                 "test@test.com");
+
+        userMapper.register(new RegisterRequestDto(
+                "user",
+                encoder.encode("user"),
+                "user",
+                "user@user.com"));
+
+        boardMapper.writeNotice(new WriteRequestDto(
+                        "Test Notice",
+                        "Test Notice Content"),
+                "test");
+
+        boardMapper.writeBoard(new WriteRequestDto(
+                        "Test Board",
+                        "Test Board Content"),
+                "user");
+
+
     }
 }
